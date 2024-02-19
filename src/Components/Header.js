01b8ea33227736cd31dc40292ae7e89e094f4e0f
Anurag Ghosekar <anuraghosekar04@gmail.com>
2024-02-19 08:28:33 +0530

    Deployment ready

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <>
     <header className="header">
         <div className="header__title" id="parallax">
-          <h1>
+          <h1 id="header--div">
             To live a {/* <!-- Green highlight effect --> */}&nbsp;
             <span className="highlight">full life, </span>
             <br />
