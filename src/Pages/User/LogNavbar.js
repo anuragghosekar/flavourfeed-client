d2f2cefddb9953bf48b06c78a5936adbd4048040
Anurag Ghosekar <anuraghosekar04@gmail.com>
2024-02-19 11:04:57 +0530

    Ready for deployment

--- src/Pages/User/LogNavbar.js
+++ src/Pages/User/LogNavbar.js
@@ -1,12 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import { Navbar, Container, Nav } from 'react-bootstrap';  
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 // import logo from '../../public/Images/Logo.png'
-import '../../Style/User/LogNavbar.css';
-import UserService from '../../Service/UserService';
-import React, { useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import "../../Style/User/LogNavbar.css";
+import UserService from "../../Service/UserService";
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
-export default function LogNavbar() {  
+export default function LogNavbar() {
   const [user, setUser] = useState(null);
   const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
   const [loggedIn, setLoggedIn] = useState(true);
@@ -14,7 +14,7 @@ export default function LogNavbar() {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userId = localStorage.getItem('userId');
+        const userId = localStorage.getItem("userId");
         if (!userId) {
           setLoggedIn(false);
         } else {
@@ -22,11 +22,11 @@ export default function LogNavbar() {
           const userData = response.data;
           setUser(response.data);
         }
-       } catch (error) {
-        console.error('Error fetching user data:', error);
-       }
-  };
-  fetchUserData();
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
   }, []);
 
   const handleLogout = () => {
@@ -35,66 +35,67 @@ export default function LogNavbar() {
 
   if (!loggedIn) {
     return <Navigate to="/login" replace />;
-    }
+  }
 
-    const confirmLogout = () => {
-      UserService.logout()
-        .then(response => {
-          localStorage.removeItem('userId');
-          console.log('User Logged Out successfully:', response.data);
-          window.location.href = '/login';
-        })
-        .catch(error => {
-          console.error('Error deleting user:', error);
-        });
-    };
-    const cancelLogout = () => {
-      setShowLogoutConfirmation(false);
-    };
+  const confirmLogout = () => {
+    UserService.logout()
+      .then((response) => {
+        localStorage.removeItem("userId");
+        console.log("User Logged Out successfully:", response.data);
+        window.location.href = "/login";
+      })
+      .catch((error) => {
+        console.error("Error deleting user:", error);
+      });
+  };
+  const cancelLogout = () => {
+    setShowLogoutConfirmation(false);
+  };
 
-
-
-  return (  
-    <>  
-    {showLogoutConfirmation && (
+  return (
+    <>
+      {showLogoutConfirmation && (
         <div className="confirmation-popup">
-          <p>Are you sure you want to Logout your account? This action cannot be undone.</p>
+          <p>
+            Are you sure you want to Logout your account? This action cannot be
+            undone.
+          </p>
           <button onClick={confirmLogout}>Yes</button>
           <button onClick={cancelLogout}>No</button>
         </div>
       )}
-    <div className='main-div'>
-      <Navbar collapseOnSelect expand="lg">  
-        <Container>  
-      <div>
-        <Navbar.Brand href="userProfile">  
-      <img  
-        src="/Images/Logo2.png"  
-        width="150px"  
-        height="100px"  
-        className="nav__logo"  
-        alt="Brand logo"  
-      />  
-      
-    </Navbar.Brand>   
-    </div> 
-    <div className='links'>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />  
-            <Navbar.Collapse id="responsive-navbar-nav">  
-              <Nav className="me-auto">  
-                <Nav.Link href="/allrecepies">All Recipes</Nav.Link>  
-                <Nav.Link href="/mealplanning">Meal Planning</Nav.Link> 
-                <Nav.Link href="/userrecipes">My Recipes</Nav.Link>
-                <Nav.Link href="/userProfile">My Profile</Nav.Link> 
-                <Nav.Link href="/feedback">Feedback</Nav.Link> 
-                <button className="logout-btn" onClick={handleLogout}>Logout</button>
-              </Nav>  
-            </Navbar.Collapse>  
-        </div>
-        </Container>  
-</Navbar>  
+      <div className="main-div">
+        <Navbar collapseOnSelect expand="lg">
+          <Container>
+            <div>
+              <Navbar.Brand href="userProfile">
+                <img
+                  src="\Images\logo2.png"
+                  width="150px"
+                  height="100px"
+                  className="nav__logo"
+                  alt="Brand logo"
+                />
+              </Navbar.Brand>
+            </div>
+            <div className="links">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="/allrecepies">All Recipes</Nav.Link>
+                  <Nav.Link href="/mealplanning">Meal Planning</Nav.Link>
+                  <Nav.Link href="/userrecipes">My Recipes</Nav.Link>
+                  <Nav.Link href="/userProfile">My Profile</Nav.Link>
+                  <Nav.Link href="/feedback">Feedback</Nav.Link>
+                  <button className="logout-btn" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          </Container>
+        </Navbar>
       </div>
-      
-    </>  
-  );  
+    </>
+  );
 }
